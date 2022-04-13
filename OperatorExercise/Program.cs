3038ea52ef420a9d74a84c0a9a8001235cf41526
Taj <tajmarie92@gmail.com>
2022-04-12 21:56:14 -0500

    Completed both exercises one and two.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -12,6 +12,22 @@ namespace OperatorExercise
             int remainder = a % b;
 
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            //Calling a method
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double bitch = AreaOfACircle(radius);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {bitch}");
+            
         }
+
+
+        //declaring a method
+        public static double AreaOfACircle(double r)
+        {
+            double area = Math.Pow(Math.PI * r, 2);
+            return area;
+        }
+
     }
 }
