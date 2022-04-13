fb24abb49bbd9efc6afdb2eb69082edd60b76aef
Taj <tajmarie92@gmail.com>
2022-04-12 20:48:40 -0500

    Add project files.

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+        }
+    }
+}
